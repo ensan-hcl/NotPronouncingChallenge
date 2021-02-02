3f89bf213e8aec25d9752db4bff23b379a81ef07
En3_HCl <En3_HCl_K@icloud.com>
2021-02-02 21:42:12 +0900

    validationの修正

--- src/models/PropertyStore.js
+++ src/models/PropertyStore.js
@@ -11,6 +11,10 @@ var PropertyStore = {
         if (this.state.property.word.length == 0) {
           throw new Error('1文字以上入力してください')
         }
+        const data = this.state.property.word.match(/[^a-zA-Z \'\.,']/gi)
+        if (data){
+            throw new Error('英単語を入力してください')
+        }
         return true
     },
 
